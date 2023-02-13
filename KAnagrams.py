98bfc1fdc4d613a531649a5a3b519c73549c1cbf
anushil7a <87778762+anushil7a@users.noreply.github.com>
2023-02-13 17:26:22 -0500

    Add files via upload

---
+++ KAnagrams.py
@@ -0,0 +1,61 @@
+#Q7
+#55 min
+def KAnagrams(string, second, k):
+    
+    #if the length of the strings do not match, return false
+    if len(string) != len(second):
+        return False
+    
+    #dict to keep track of how many times a letter popped up in string one
+    temp1 = {}
+    
+    #dict to keep track of how many times a letter popped up in string two
+    temp2 = {}
+    
+    for i in string:
+        #checks how many times a letter in string one, also pops up in string two
+        if i in second:
+            #if it pops up in both add it to temp1
+            if i not in temp1:
+                temp1[i] = 1
+            else:
+                #if it is already in temp1 incrase the value by one
+                temp1[i] +=1
+                
+        #if it appears in string one but does not appear in string two, -1 from k,
+        #because that is how man letters we are allowed to switch
+        else:
+            k -=1
+    
+    for i in second:
+        #checks how many times a letter in string two, also pops up in string one
+        if i in string:
+            if i not in temp2:
+                temp2[i] = 1
+            else:
+                temp2[i] +=1
+    
+    #print("temp1: "+str(temp1))
+    #print("temp2: "+str(temp2))
+    
+    #if the value of temp1 is bigger than temp2,
+    #check if temp2 values +k is equal to temp1 value
+    #if it is not return false
+    if sum(temp1.values()) > sum(temp2.values()):
+        return sum(temp1.values()) == sum(temp2.values())+k
+    
+    #same thing as the top but we are checking if temp2 value is bigger than temp1
+    elif sum(temp1.values()) < sum(temp2.values()):
+        return sum(temp2.values()) == sum(temp1.values())+k
+    
+    #if the values are the same, add k to one of them (doesnt matter which one cuz k should be zero for it to return true)
+    #if k is not 0 it would be false
+    return sum(temp1.values())+k == sum(temp2.values())
+
+
+
+#print(KAnagrams("apple", "peach", 1))
+#print(KAnagrams("apple", "peach", 2))
+#print(KAnagrams("cat", "dog", 3))
+#print(KAnagrams("debit curd", "bad credit", 1))
+#print(KAnagrams("baseball", "basketball", 1))
---
+++ MaxMeanSubArray.py
@@ -0,0 +1,26 @@
+#Fixed-size sliding window
+#35 min
+def MaxMeanSubArray(lst,size):
+    mean = 0
+    
+    temp = size
+    
+    #create a range of input list-size+one
+    #this range adjusts for all the numbers in the list
+    for i in range(len(lst)-size+1):
+        
+        #if sum from lst[1] to lst[temp] is greater than mean, change mean value to this
+        if (sum(lst[i:temp:1]))/size > mean:
+            mean = sum(lst[i:temp:1])/size
+            #increase temp by 1 if it does
+            temp +=1
+        else:
+            #increase temp by 1 if it doesnt too
+            temp +=1
+            
+    return mean
+
+#print(MaxMeanSubArray([4, 5, -3, 2, 6, 1], 2))
+#print(MaxMeanSubArray([4, 5, -3, 2, 6, 1], 3))
+#print(MaxMeanSubArray([1, 1, 1, 1, -1, -1, 2, -1, -1], 3))
+#print(MaxMeanSubArray([1, 1, 1, 1, -1, -1, 2, -1, -1, 6], 5))
