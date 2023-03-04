6b96ab2c137c72dc1eadb0907c5a51a139013db2
anushil7a <87778762+anushil7a@users.noreply.github.com>
2023-03-04 18:49:14 -0500

    Add files via upload

---
+++ TwoSum.py
@@ -0,0 +1,27 @@
+#10 time spent: 20 min
+#method used: pointer method
+#questions: is test two wrong in the examples because I dont get why it would return 4
+
+def TwoSum(arr, target):
+    counter = 0
+    firstPointer = 0
+    lastPointer = len(arr)-1
+    arr.sort()
+    
+    while(firstPointer < lastPointer):
+        if(arr[firstPointer] + arr[lastPointer] == target):
+            counter +=1
+            lastPointer -=1
+        else:
+            lastPointer -=1
+        
+        if(firstPointer == lastPointer):
+            firstPointer +=1
+            lastPointer = len(arr)-1
+    
+    return counter
+
+##print(TwoSum([1, 10, 8, 3, 2, 5, 7, 2, -2, -1], 10))
+##print(TwoSum([1, 10, 8, 3, 2, 5, 7, 2, -2, -1], 8))
+##print(TwoSum([4, 3, 3, 5, 7, 0, 2, 3, 8, 6], 6))
+##print(TwoSum([4, 3, 3, 5, 7, 0, 2, 3, 8, 6], 1))
