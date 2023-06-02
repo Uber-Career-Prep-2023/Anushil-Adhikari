1dc6b3d03d227d8bdbe0a30dc8b9d7dd56835fee
anushil7a <87778762+anushil7a@users.noreply.github.com>
2023-06-02 15:27:29 -0400

    Add files via upload

---
+++ HW3/NumberOfIslands.py
@@ -0,0 +1,44 @@
+"""
+Question 4: NumberOfIslands
+Given a binary matrix in which 1s represent land and 0s represent water.
+Return the number of islands (contiguous 1s surrounded by 0s or the edge of the matrix).
+
+type: graph problem
+time complex: O(n*m)
+space complex: O(n*m)
+approach: I used dfs recursion method to solve this problem
+time spent: 35 minutes
+"""
+
+def NumberOfIslands(array):
+    if not array:
+        return 0
+    
+    islands = 0
+    
+    def dfs(array, row, col):
+        array[row][col] = "2"
+        directions = [[row-1, col],[row+1, col], [row, col-1], [row, col+1]]
+        #print(array)
+        for row, col in directions:
+            #print(row, col)
+            if row >=0 and col >=0 and row < len(array) and col < len(array[row]) and array[row][col] == "1":
+                dfs(array, row, col)
+            
+    
+    for row in range(len(array)):
+        for col in range(len(array[row])):
+            if array[row][col] == "1":
+                dfs(array, row, col)
+                islands +=1
+            
+    return islands
+
+#print("hello")
+grid1 = [["1","1","0","0","0"]]
+#print(grid1[0][1])
+grid = [["1","1","0","0","0"],
+        ["1","1","0","0","0"],
+        ["0","0","1","0","0"],
+        ["0","0","0","1","1"]]
+print(NumberOfIslands(grid))
