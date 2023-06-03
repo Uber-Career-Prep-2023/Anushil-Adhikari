fba52f69630ee52e9b92aa3cace9a1ebe7ae8e73
anushil7a <87778762+anushil7a@users.noreply.github.com>
2023-06-03 19:56:59 -0400

    Delete FirstKBinaryNumbers.py

--- HW3/FirstKBinaryNumbers.py
+++
@@ -1,9 +0,0 @@
-def FirstKBinaryNumbers(num):
-    temp = []
-    for i in range(num):
-        bob = bin(i)[2::]
-        temp.append(bin(i)[2::])
-    
-    return temp
-
-print(FirstKBinaryNumbers(10))
